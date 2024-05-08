7ad1f92caadca997700b7a884442fc66072dfcc3
pujavs <43700552+pujavs@users.noreply.github.com>
2024-05-08 12:48:43 +0530

    feat: update UserHelperService.java

--- projects/saml-Inbound-saml/lib/org/gluu/agama/saml/service/UserHelperService.java
+++ projects/saml-Inbound-saml/lib/org/gluu/agama/saml/service/UserHelperService.java
@@ -9,16 +9,15 @@ import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class UserHelperService {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(UserHelperService.class);
 
-public static String exec(String prefix, String attr, Map<String, Object> profile) {
-    logger.debug("Retrieving user identified with prefix:{}, attr:{}, profile:{}", prefix, attr, profile);
+    public static String exec(String prefix, String attr, Map<String, Object> profile) {
+        logger.debug("Retrieving user identified with prefix:{}, attr:{}, profile:{}", prefix, attr, profile);
         String uid = Optional.ofNullable(prefix).orElse("") + profile.get(attr).toString();
-        
-        //if not existing, insert user with the uid just built
+
+        // if not existing, insert user with the uid just built
         UserService userService = CdiUtil.bean(UserService.class);
         logger.debug("Retrieving user identified by {}", uid);
         User user = userService.getUser(uid);
@@ -32,28 +31,28 @@ public static String exec(String prefix, String attr, Map<String, Object> profil
             userService.addUser(user, true);
         }
         return uid;
-        
+
     }
 
-public static String exec(String name) {
-    logger.info("\n\n\n Check user identified by name:{}", name);
-    String uid = name;
-    
-    //if not existing, insert user with the uid just built
-    UserService userService = CdiUtil.bean(UserService.class);
-    logger.debug("\n\n Retrieving user identified by {}", uid);
-    User user = userService.getUser(uid);
-    logger.info("\n\n user identified by {} is {}", uid, user);
-    if (user != null) {
-        logger.info("Found user:{}!",user);
-    } else {
-        logger.info("\n\n\n Not found. Inserting entry");
-        user = new User();
-        user.setUserId(uid);
-        userService.addUser(user, true);
+    public static String exec(String name, Map samlResponse) {
+        logger.info("\n\n\n Check user identified by name:{}, samlResponse:{}", name, samlResponse);
+        String uid = name;
+
+        // if not existing, insert user with the uid just built
+        UserService userService = CdiUtil.bean(UserService.class);
+        logger.debug("\n\n Retrieving user identified by {}", uid);
+        User user = userService.getUser(uid);
+        logger.info("\n\n user identified by {} is {}", uid, user);
+        if (user != null) {
+            logger.info("Found user:{}!", user);
+        } else {
+            logger.info("\n\n\n Not found. Inserting entry");
+            user = new User();
+            user.setUserId(uid);
+            userService.addUser(user, true);
+        }
+        return uid;
+
     }
-    return uid;
-    
-}
 
 }
