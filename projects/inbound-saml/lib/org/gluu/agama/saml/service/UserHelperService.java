8911604bbd71accb79d0ed37366bda3065c12f22
pujavs <43700552+pujavs@users.noreply.github.com>
2024-05-09 21:53:41 +0530

    feat: add UserHelperService.java

---
+++ projects/inbound-saml/lib/org/gluu/agama/saml/service/UserHelperService.java
@@ -0,0 +1,37 @@
+package org.gluu.agama.saml.service;
+
+import io.jans.as.common.model.common.User;
+import io.jans.as.common.service.common.UserService;
+import io.jans.service.cdi.util.CdiUtil;
+
+import java.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserHelperService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserHelperService.class);
+
+    public static String exec(String prefix, String attr, Map<String, Object> profile) {
+        logger.debug("Retrieving user identified with prefix:{}, attr:{}, profile:{}", prefix, attr, profile);
+       // String uid = Optional.ofNullable(prefix).orElse("") + profile.get(attr).toString();
+        String uid = prefix;
+        // if not existing, insert user with the uid just built
+        UserService userService = CdiUtil.bean(UserService.class);
+        logger.debug("Retrieving user identified by {}", uid);
+        User user = userService.getUser(uid);
+        logger.debug("user identified by {} is {}", uid, user);
+        if (user != null) {
+            logger.debug("Found!");
+        } else {
+            logger.debug("Not found. Inserting entry");
+            user = new User();
+            user.setUserId(uid);
+            userService.addUser(user, true);
+        }
+        return uid;
+
+    }
+
+}
